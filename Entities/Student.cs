0dbda02991400017ce6601467df5fe5e16bd51a9
Armando Rodriguez <rodriguezdiazag@gmail.com>
2023-09-12 12:25:15 -0600

    Creates and save a card with student info and overlay their QRCode.

--- Entities/Student.cs
+++ Entities/Student.cs
@@ -8,12 +8,12 @@ namespace QRGenerator.Entities
 {
     public class Student
     {
-       public int Id { get; set; }
+       public string? Id { get; set; }
         public string? Name { get; set; }
         
-        public int Age { get; set; }
-        public int Grade { get; set; }
+        public string? Age { get; set; }
+        public string? Grade { get; set; }
         public string? Gender { get; set; }
-        public bool HasQr { get; set; } =false;
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,105 +1,215 @@
 ﻿using QRGenerator.Entities;
-using QRCoder;
+using Microsoft.Office.Interop.Excel;
 using System.Drawing;
+using System.Drawing.Imaging;
+
+using QRCoder;
+using ZXing;
+using ZXing.QrCode;
+
 
 namespace QRGenerator
 {
     internal class Program
     {
-        const string path = "D:\\Users\\Descargas\\Calificaciones (2)";
+        #region properties
+        private const string excelPath = "D:\\Users\\Descargas\\Calificaciones (2)";
+        private const string imagePath = $"D:\\Users\\Projects\\QRGenerator\\StudentsQR\\";
+        private const string cardPath = $"D:\\Users\\Projects\\QRGenerator\\Card\\";
+        private static readonly Microsoft.Office.Interop.Excel.Application excelApp = new();
+        private static Workbook? workBook;
+        private static Worksheet? WorkSheet;
+        #endregion
+
         static void Main(string[] args)
         {
             Console.WriteLine("Iniciando Programa");
-             List<Student> students = GetStudents();
+
+            List<Student> students = GetStudents();
             int row = 1;
             foreach (Student student in students)
             {
-                
-                student.HasQr = GenerateQR(student);
-                if (student.HasQr)
-                {
-                    UpdateExcel(student, row);
-                    Console.WriteLine("QR creado para: {0}", student.Name);
-                    row++;
-                }
+
+
+
+                SaveCard(student);
+                Console.WriteLine("QR creado para: {0}", student.Name);
+                row++;
+
 
             }
+
+            CloseWorkBook();
+
         }
-        public static List<Student> GetStudents()
+        internal static List<Student> GetStudents()
         {
             Console.WriteLine("Obteniendo Estudiantes.....");
             List<Student> students = new();
-           
+
             //Get data from excel and store it in a list of students.
-            var excelApp = new Microsoft.Office.Interop.Excel.Application
-            {
-                Visible = true
-            };
-            var workBook = excelApp.Workbooks.Open(path);
-            var WorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets.get_Item(1);
+
+            workBook = excelApp.Workbooks.Open(excelPath);
+            WorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets.get_Item(1);
             Console.WriteLine("Lista de estudiantes: ");
-            for (int i = 1; i <= WorkSheet.Rows.Count; i++)
+            int j = 0;
+            for (int i = 2; i <= WorkSheet.Rows.Count; i++)
             {
-                students.Add(new Student()
+                if (!string.IsNullOrEmpty(WorkSheet.Cells[i, "A"].Text))
                 {
-                    Id = (int)WorkSheet.Cells[i, 0],
-                    Name = (string)WorkSheet.Cells[i, 1],
-                    Age = (int)WorkSheet.Cells[i, 2],
-                    Grade = (int)WorkSheet.Cells[i, 3],
-                    Gender = (string)WorkSheet.Cells[i, 4],
-                });
-                Console.WriteLine(students[i].Name);
+                    students.Add(new()
+                    {
+                        Id = WorkSheet.Cells[i, "A"].Text,
+                        Name = WorkSheet.Cells[i, "B"].Text,
+                        Age = WorkSheet.Cells[i, "C"].Text,
+                        Grade = WorkSheet.Cells[i, "D"].Text,
+                        Gender = WorkSheet.Cells[i, "E"].Text
+                    });
+
+                    Console.WriteLine(students[j].Name);
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+
             }
+
+            return students;
+        }
+        internal static void CloseWorkBook()
+        {
+
             workBook.Close();
             excelApp.Workbooks.Close();
             excelApp.Quit();
-            return students;
         }
-        public static bool GenerateQR(Student item)
+        internal static Bitmap GenerateQR(Student item)
         {
             try
             {
                 //Generate the QR code
+                string? imageName = item.Id.ToString() + item.Name + ".jpg";
+                string data = Newtonsoft.Json.JsonConvert.SerializeObject(item);//creates JSON with student data
+
+
                 QRCodeGenerator generator = new QRCodeGenerator();
                 QRCodeData codeData;
 
-                codeData = generator.CreateQrCode(item.ToString(), QRCodeGenerator.ECCLevel.Q);
-                PngByteQRCode qrCode = new PngByteQRCode(codeData);
+                codeData = generator.CreateQrCode(data, QRCodeGenerator.ECCLevel.L);
+               // PngByteQRCode qrCode = new PngByteQRCode(codeData);
+                QRCoder.BitmapByteQRCode qrCode = new QRCoder.BitmapByteQRCode(codeData);
 
-                byte[] qrCodeByteArr = qrCode.GetGraphic(20);
+                byte[] qrCodeByteArr = qrCode.GetGraphic(4);
 
                 //Save the qr in a PNG file 
                 using var ms = new MemoryStream(qrCodeByteArr);
 
-                Image image = Image.FromStream(ms);
-                string? imageName = item.Id.ToString() + item.Name + ".jpg";
-                image.Save(imageName, System.Drawing.Imaging.ImageFormat.Jpeg);
-                imageName = string.Empty;
+                Bitmap bitmap = new Bitmap(ms);
+                
 
-                return true;
+                //var qr = new QRCodeWriter();
+                //var matrix = qr.encode(data, BarcodeFormat.QR_CODE, 100, 100);
+                //var writer = new ZXing.Windows.Compatibility.BarcodeWriter();
+                //using var bitmap = writer.Write(matrix);
+
+                return bitmap;
             }
             catch (Exception e)
             {
                 Console.WriteLine("No fue posible crear el codigo QR debido a {0}", e.Message);
-                return false;
+                throw;
 
             }
         }
-        public static void UpdateExcel(Student student,int row)
+
+
+        internal static string GetHTML(string name, string id, string QRPath)
         {
-            //update has qr field in the excel and save it
-            var excelApp = new Microsoft.Office.Interop.Excel.Application
-            {
-                Visible = true
-            };
-            var workBook = excelApp.Workbooks.Open(path);
-            var WorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets.get_Item(1);
-            WorkSheet.Cells[row, 4] = student.HasQr?"QR creado":"Sin QR creado";
-            workBook.Save();
-            workBook.Close();
-            excelApp.Workbooks.Close();
-            excelApp.Quit();
+            string html = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'>    <meta name='viewport' content='width=device-width, initial-scale=1.0'>    <title>Credencial de Estudiante con Código QR</title>    <style>        body {            font-family: Arial, sans-serif;            background-color: #f0f0f0;        }        .card {            width: 300px;            background-color: #fff;            border-radius: 10px;            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);            margin: 20px auto;            padding: 20px;        }        .student-photo {            width: 100px;            height: 100px;            border-radius: 50%;            margin: 0 auto 10px;            background-color: #ccc;            background-image: url('./Card/template/student.png'); /* Reemplaza 'tu_foto_de_estudiante.jpg' con la URL de tu foto */            background-size: cover;            background-position: center;        }        .student-info {            text-align: center;        }        .student-name {            font-size: 24px;            font-weight: bold;            margin: 10px 0;        }        .student-id {            font-size: 16px;            margin-bottom: 10px;        }        .validity {            font-size: 12px;            color: #888;        }        .qr-code {            width: 100px;            height: 100px;            margin: 10px auto;        }        img{            width : 80%;            height: 80%;        }    </style></head><body>    <div class='card'>        <div class='student-photo'>                    </div>        <div class='student-info'>            <div class='student-name'>{StudentName}</div>            <div class='student-id'>ID de Estudiante: {StudentId}</div>            <div class='validity'>Válido hasta: 31 de diciembre de 2023</div>        </div>        <div class='qr-code'>            <img src='{QRCode}' alt='Código QR'>        </div>    </div></body></html>\"";
+            html = html.Replace("{StudentName}", name);
+            html = html.Replace("{StudentId}", id);
+            html = html.Replace("{QRCode}", QRPath);
+            return html;
 
         }
+        internal static void SaveCard(Student student)
+        {
+            string studentQRPath = Path.Combine(imagePath, string.Concat(student.Id, student.Name.Replace(" ", ""), ".png"));
+            string HTMLContent = GetHTML(student.Name, student.Id, studentQRPath);
+            GenerateCard(string.Concat(student.Id, student.Name.Replace(" ", "_"), "Card.JPEG"), student);
+
+        }
+        static void GenerateCard(string HTMLContent, string FileName)
+        {
+            try
+            {
+                Console.WriteLine("Generando credencial");
+                // Create a PDF document from the HTML template
+
+                var renderer = new IronPdf.HtmlToPdf();
+                var pdf = renderer.RenderHtmlAsPdf(HTMLContent);
+                pdf.SaveAs(cardPath + FileName);
+
+
+                //Creates a MS and save the card as image
+
+                //using var ms = new MemoryStream(pdf.Stream.ToArray());
+                //System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+                //image.Save(Path.Combine(imagePath, FileName));
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        static void GenerateCard(string FileName, Student item)
+        {
+            try
+            {
+                System.Drawing.Image templateImage = System.Drawing.Image.FromFile("D:\\Users\\Projects\\QRGenerator\\Card\\template\\StudentCard2.PNG");
+
+                using (Graphics graphics = Graphics.FromImage(templateImage))
+                {
+                    // Create a font and brush for the text
+                    System.Drawing.Font font = new System.Drawing.Font("Roboto", 12, FontStyle.Bold);
+                    SolidBrush brush = new SolidBrush(System.Drawing.Color.Black);
+
+                    // Define the positions to overlay student data
+                    System.Drawing.Point studentNamePosition = new System.Drawing.Point(30, 140);
+                    System.Drawing.Point studentIDPosition = new System.Drawing.Point(80, 170);
+                    System.Drawing.Point QrPosition = new System.Drawing.Point(100, 215);
+
+
+                    // Draw student data on the template image
+                    graphics.DrawString("Nombre: " + item.Name, font, brush, studentNamePosition);
+                    graphics.DrawString("Matricula: " + item.Id, font, brush, studentIDPosition);
+
+
+                    // Overlay the QR code
+                    Bitmap qrCode =  GenerateQR(item); // Adjust QR code size as needed
+                    graphics.DrawImage(qrCode, QrPosition);
+
+                    // Dispose of the font and brush
+                    font.Dispose();
+                    brush.Dispose();
+                }
+                templateImage.Save(cardPath + FileName);
+                templateImage.Dispose();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
